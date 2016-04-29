b813089f7d7994b3c5d073cb93a08958119560c9
Saket <saketv02@gmail.com>
2016-04-29 08:44:23 -0700

    resolved closure bugs

--- src/word2vec.scala
+++ src/word2vec.scala
@@ -9,57 +9,52 @@ import org.apache.spark.mllib.clustering.{KMeans,KMeansModel}
 import org.apache.spark.mllib.feature.IDF
 import org.apache.spark.mllib.feature.{Word2Vec,Word2VecModel}
 import org.apache.spark.mllib.feature.Normalizer
-
+import scala.collection.immutable.StringLike
 
 
 object word2vec {
   
+  
+    
    def main(numClusters:Int,collection:String) {
-   val sc = new SparkContext("local[2]", "Clustering")
-   val users = sc.textFile("user/cs5604s16_sn/input/tweets/" + collection +".txt" )
+  // val sc = new SparkContext("local[2]", "Clustering")
+   val users = sc.textFile("input/tweets" + collection +".txt" )
 
    users.first()
    
    val filtereddata = users.filter(!_.isEmpty).map(line=>line.split("\t",-1)).map(line=>(line(0),line(1).split(" ").filter(_.nonEmpty)))
    val corpus =  users.filter(!_.isEmpty).map(line=>line.split("\t",-1)).map(line=>line(1).split(" ").toSeq)
-   
+    
    
    val values:RDD[Seq[String]] = filtereddata.map(s=>s._2.filter(_.nonEmpty))
+   
    val keys = filtereddata.map(s=>s._1)
    
   
    println(corpus.take(1).mkString("\n"))
-   /*
-   val hashingTF:RDD[Vector] = new HashingTF().transform(values)
-   val idf= new IDF(minDocFreq=10).fit(hashingTF)
-   val tfidf:RDD[Vector]= idf.transform(hashingTF).cache()
-   */
-   def sumArray (m: Array[Double], n: Array[Double]): Array[Double] = {
-		   	for (i <- 0 until m.length) {m(i) += n(i)}
-		   	return m
-   }
-
-   def divArray (m: Array[Double], divisor: Double) : Array[Double] = {
-		   for (i <- 0 until m.length) {m(i) /= divisor}
-		   return m
-   }
-
-   def wordToVector (w:String, m: Word2VecModel): Vector = {
-		   try {
-			   return m.transform(w)
-		   } catch {
-		   case e: Exception => return Vectors.zeros(100)
-		   }  
-   }
-   
+  
+  
    
    val vec = new Word2Vec()
    val model = vec.fit(corpus)
+   println("fitting done......................................")
+   println(values.take(2).mkString("\n"))
    
-  // val test =  values.map(x=> new DenseVector(x.map(m=>wordToVector(m,model).toArray).reduceLeft(sumArray)).asInstanceOf[Vector])
-  // test.count()
-   val test =  values.map(x=> (x.map(m=>wordToVector(m,model).toArray))).filter(_.length!=0).map(x=> new DenseVector(divArray(x.reduce(sumArray),x.length)).asInstanceOf[Vector])
-   
+   val outtest:RDD[Seq[Vector]]= values.map(x=>x.map(m=>try {
+		     model.transform(m)
+		   } catch {  
+		   case e: Exception => Vectors.zeros(100)//return Vectors.zeros(100)
+		   }))
+		   
+  val convertest = outtest.map(m=>m.map(x=>(x.toArray)))
+  val reducetest = convertest.map(x=>x.reduce((a,b)=>a.zip(b).map(t=>t._1+t._2)))
+  
+  val filtertest = reducetest.map(x=>x.map(m=>(m,x.length)).map(m=>m._1/m._2))
+  
+  val test = filtertest.map(x=>new DenseVector(x).asInstanceOf[Vector])
+		   
+		   
+   println(test.take(10).mkString("\n"))
    val normalizer =  new Normalizer()
    val data1= test.map(x=>(normalizer.transform(x)))
    
@@ -74,7 +69,7 @@ object word2vec {
    val wsse = clusters.computeCost(data1)
    
    
-   result.saveAsTextFile("/user/cs5604s16_sn/output/tweets/"+collection)
+   result.saveAsTextFile("output/tweets"+collection)
    println(result.take(4).mkString(" "))
    println(clustercount.take(numClusters).mkString(" "))
    println(wsse.toString)
